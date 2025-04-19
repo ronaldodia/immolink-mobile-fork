3c6b2cc987801324e5b64a11d63c50148c06e12e
ronaldodia <alassane-mpf@live.fr>
2025-04-19 13:33:39 -0400

    Update config.dart

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,9 +1,9 @@
 class Config {
-  static const String baseUrlApp = 'https://daar.server1.digissimmo.org/mobile';
-  static const String chatHostApi='chat.digissimmo.org';//'chat.digissimmo.org';
-  static const String initUrl = 'https://daar.server1.digissimmo.org';
+  static const String baseUrlApp = 'https://api.immo-place.net/mobile';
+  static const String chatHostApi='chat.immo-place.net';//'chat.digissimmo.org';
+  static const String initUrl = 'https://api.immo-place.net';
   static const String baseUrlAppLocalhost = 'http://192.168.100.6:8000/mobile';
-  static const String baseUrlSIG = 'https://gis.digissimmo.org/api';
+  static const String baseUrlSIG = 'https://gis.immo-place.net/api';
   static const String appLoginTitle = "Welcome back,";
   static const String registerTitle = "Let's create your account";
   static const String appLoginSubTitle = "Discover limitless choices and Unmatched Convenience.";
